ce867949d33e1da660d9ee4bcde6b825e9c744df
Prem Patel <80719066+sazncode@users.noreply.github.com>
2022-10-16 19:03:36 -0400

    Create FreeTacosNBurritos.js

---
+++ TacoBell/FreeTacosNBurritos.js
@@ -0,0 +1,207 @@
+// --------------------------------------
+// Information of recording
+// Time: 2022-10-10 11:15:32
+// Resolution: 1242, 2688
+// Front most app: Taco Bell
+// Orientation of front most app: Portrait
+// --------------------------------------
+
+// Purpose: To automate the process of creating a new Rewards account with Taco Bell
+// Getting a free burrito, taco, or other item 30 minutes after signing up.
+// Paired well with Slices, allowing for multiple accounts.
+
+const { touchDown, touchMove, touchUp, usleep, appActivate, keyDown, keyUp } = at
+
+appActivate("com.tacobell.ARgame");
+usleep(82456.17);
+
+touchDown(3, 816.69, 2523.45);
+usleep(82456.17);
+touchUp(3, 816.69, 2523.45);
+usleep(1710946.29);
+
+touchDown(2, 397.22, 2213.04);
+usleep(182366.67);
+touchUp(2, 397.22, 2213.04);
+usleep(1095987.17);
+
+touchDown(1, 590.53, 1897.79);
+usleep(107492.00);
+touchUp(1, 590.53, 1897.79);
+usleep(1079156.12);
+
+touchDown(5, 857.29, 2641.65);
+usleep(107800.54);
+touchUp(5, 857.29, 2641.65);
+usleep(1727213.17);
+
+touchDown(6, 760.63, 1780.53);
+usleep(132598.75);
+touchUp(6, 760.63, 1780.53);
+usleep(1128904.25);
+
+touchDown(4, 223.25, 1590.26);
+usleep(74506.42);
+touchUp(4, 223.25, 1590.26);
+usleep(1170435.38);
+
+touchDown(3, 119.83, 2195.73);
+usleep(82969.62);
+touchUp(3, 119.83, 2195.73);
+usleep(107951.71);
+
+touchDown(3, 122.73, 2166.90);
+usleep(82902.46);
+touchUp(3, 122.73, 2166.90);
+usleep(813894.88);
+
+touchDown(2, 119.83, 2033.31);
+usleep(74097.12);
+touchUp(2, 119.83, 2033.31);
+usleep(423581.08);
+
+touchDown(1, 959.74, 2044.83);
+usleep(82594.50);
+touchUp(1, 959.74, 2044.83);
+usleep(2482513.88);
+
+touchDown(4, 219.38, 1593.13);
+usleep(99260.62);
+touchUp(4, 219.38, 1593.13);
+usleep(888235.12);
+
+touchDown(3, 143.03, 2227.44);
+usleep(74518.08);
+touchUp(3, 143.03, 2227.44);
+usleep(83570.38);
+
+touchDown(3, 137.23, 2210.13);
+usleep(74162.00);
+touchUp(3, 137.23, 2210.13);
+usleep(598056.29);
+
+touchDown(5, 1152.08, 2043.89);
+usleep(91007.00);
+touchUp(5, 1152.08, 2043.89);
+usleep(539902.21);
+
+touchDown(6, 317.97, 1871.83);
+usleep(66243.04);
+touchUp(6, 317.97, 1871.83);
+usleep(82979.62);
+
+touchDown(6, 326.67, 1863.18);
+usleep(65291.96);
+touchUp(6, 326.67, 1863.18);
+usleep(2342312.46);
+
+touchDown(4, 224.21, 1600.84);
+usleep(90957.04);
+touchUp(4, 224.21, 1600.84);
+usleep(697564.71);
+
+touchDown(4, 230.01, 1573.89);
+usleep(49388.75);
+touchUp(4, 230.01, 1573.89);
+usleep(1204389.04);
+
+touchDown(3, 123.70, 2220.71);
+usleep(74031.17);
+touchUp(3, 123.70, 2220.71);
+usleep(498417.92);
+
+touchDown(2, 115.96, 2011.20);
+usleep(74489.58);
+touchUp(2, 115.96, 2011.20);
+usleep(174648.67);
+
+touchDown(1, 245.48, 2029.45);
+usleep(89970.29);
+touchUp(1, 245.48, 2029.45);
+usleep(424786.75);
+
+touchDown(5, 1004.20, 2077.52);
+usleep(99276.79);
+touchUp(5, 1004.20, 2077.52);
+usleep(282528.79);
+
+touchDown(6, 1005.16, 2195.73);
+usleep(90765.12);
+touchUp(6, 1005.16, 2195.73);
+usleep(265839.83);
+
+touchDown(4, 254.18, 1933.35);
+usleep(57896.04);
+touchMove(4, 259.01, 1921.83);
+usleep(7973.04);
+touchMove(4, 264.81, 1921.83);
+usleep(8712.29);
+touchMove(4, 269.64, 1921.83);
+usleep(8338.46);
+touchMove(4, 276.41, 1921.83);
+usleep(8486.88);
+touchUp(4, 292.84, 1915.10);
+usleep(539649.75);
+
+touchDown(3, 859.22, 2220.71);
+usleep(57718.96);
+touchMove(3, 847.62, 2220.71);
+usleep(8396.58);
+touchMove(3, 857.29, 2220.71);
+usleep(8653.67);
+touchMove(3, 867.92, 2225.51);
+usleep(7768.08);
+touchMove(3, 881.45, 2231.29);
+usleep(8494.62);
+touchUp(3, 899.81, 2243.80);
+usleep(157977.50);
+
+touchDown(2, 1017.73, 1893.94);
+usleep(74409.62);
+touchUp(2, 1017.73, 1893.94);
+usleep(274433.00);
+
+touchDown(1, 230.01, 2341.83);
+usleep(90724.25);
+touchUp(1, 230.01, 2341.83);
+usleep(315975.83);
+
+touchDown(5, 621.45, 1857.43);
+usleep(99357.29);
+touchUp(5, 621.45, 1857.43);
+usleep(166434.12);
+
+touchDown(6, 840.86, 1896.85);
+usleep(82598.42);
+touchUp(6, 840.86, 1896.85);
+usleep(1328718.42);
+
+touchDown(4, 764.50, 2191.88);
+usleep(74441.29);
+touchMove(4, 760.63, 2203.40);
+usleep(8003.54);
+touchMove(4, 767.40, 2203.40);
+usleep(8505.00);
+touchMove(4, 776.10, 2203.40);
+usleep(8286.83);
+touchUp(4, 791.56, 2206.31);
+usleep(1959920.33);
+
+touchDown(3, 1153.04, 1602.73);
+usleep(107569.54);
+touchUp(3, 1153.04, 1602.73);
+usleep(2706549.12);
+
+touchDown(2, 783.83, 2000.62);
+usleep(123854.50);
+touchUp(2, 783.83, 2000.62);
+usleep(1661347.50);
+
+touchDown(1, 152.69, 1958.33);
+usleep(82329.29);
+touchUp(1, 152.69, 1958.33);
+usleep(1370944.79);
+
+touchDown(5, 848.59, 2515.78);
+usleep(98814.33);
+touchUp(5, 848.59, 2515.78);
