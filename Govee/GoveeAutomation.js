a9c80911d87f2959385d6c76009cdff4832af7e4
Prem Patel <80719066+sazncode@users.noreply.github.com>
2022-10-16 18:45:48 -0400

    Updating Spacing

--- Govee/GoveeAutomation.js
+++ Govee/GoveeAutomation.js
@@ -1,224 +1,140 @@
 // --------------------------------------
-
 // Information of recording
-
-// Time: 2022-09-26 08:30:33
-
-// Resolution: 1242, 2688
-
 // Front most app: Govee Home
-
 // Orientation of front most app: Portrait
-
 // --------------------------------------
 
+// Purpose: Automate Goove comments and video uploads daily. The script relies on videos being added to the first allbum on iOS.  
 const { touchDown, touchMove, touchUp, usleep, appActivate, keyDown, keyUp } = at
 
 appActivate("com.ihoment.GoVeeSensor");
 
+
 touchDown(3, 601.16, 2478.29);
-
 usleep(132523.29);
-
 touchUp(3, 601.16, 2478.29);
-
 usleep(1030309.58);
 
 touchDown(3, 602.12, 2482.14);
-
 usleep(98305.83);
-
 touchUp(3, 602.12, 2482.14);
-
 usleep(458193.46);
 
 touchDown(3, 595.36, 2478.29);
-
 usleep(90925.38);
-
 touchUp(3, 595.36, 2478.29);
-
 usleep(1122111.38);
 
 touchDown(4, 361.46, 371.60);
-
 usleep(174264.88);
-
 touchUp(4, 361.46, 371.60);
-
 usleep(714325.08);
 
 touchDown(4, 357.59, 373.53);
-
 usleep(124826.12);
-
 touchUp(4, 357.59, 373.53);
-
 usleep(1478804.75);
 
 touchDown(1, 680.41, 775.24);
-
 usleep(157832.75);
-
 touchUp(1, 680.41, 775.24);
-
 usleep(747328.54);
 
 touchDown(1, 601.16, 783.89);
-
 usleep(157821.12);
-
 touchUp(1, 601.16, 783.89);
-
 usleep(1860557.92);
 
 touchDown(6, 1163.67, 1896.85);
-
 usleep(156698.92);
-
 touchUp(6, 1163.67, 1896.85);
-
 usleep(1222031.21);
 
 touchDown(3, 587.63, 2511.92);
-
 usleep(106702.38);
-
 touchUp(3, 587.63, 2511.92);
-
 usleep(1055862.92);
 
 touchDown(2, 871.78, 2138.06);
-
 usleep(99259.75);
-
 touchUp(2, 871.78, 2138.06);
-
 usleep(83586.04);
 
 touchDown(5, 925.91, 1905.46);
-
 usleep(91333.12);
-
 touchUp(5, 925.91, 1905.46);
-
 usleep(8057.67);
 
 touchDown(4, 490.01, 2174.57);
-
 usleep(57330.54);
-
 touchUp(4, 490.01, 2174.57);
-
 usleep(142455.62);
 
 touchDown(1, 317.97, 1884.34);
-
 usleep(66102.00);
-
 touchUp(1, 317.97, 1884.34);
-
 usleep(589781.71);
 
 touchDown(6, 1183.00, 1584.47);
-
 usleep(91224.88);
-
 touchUp(6, 1183.00, 1584.47);
-
 usleep(1378876.08);
 
 touchDown(3, 1166.57, 661.83);
-
 usleep(123562.58);
-
 touchUp(3, 1166.57, 661.83);
-
 usleep(1022638.04);
 
 touchDown(2, 102.43, 235.14);
-
 usleep(82915.92);
-
 touchUp(2, 102.43, 235.14);
-
 usleep(1163486.25);
 
 touchDown(5, 1146.28, 1744.97);
-
 usleep(123249.50);
-
 touchUp(5, 1146.28, 1744.97);
-
 usleep(881526.96);
 
 touchDown(4, 757.73, 2261.07);
-
 usleep(132157.04);
-
 touchUp(4, 757.73, 2261.07);
-
 usleep(1653772.46);
 
 touchDown(2, 96.63, 233.21);
-
 usleep(90383.92);
-
 touchUp(2, 96.63, 233.21);
-
 usleep(1405261.42);
 
 touchDown(1, 474.54, 1319.23);
-
 usleep(165952.88);
-
 touchUp(1, 474.54, 1319.23);
-
 usleep(1104854.62);
 
 touchDown(6, 338.26, 341.82);
-
 usleep(115943.38);
-
 touchUp(6, 338.26, 341.82);
-
 usleep(947284.42);
 
 touchDown(3, 259.01, 401.38);
-
 usleep(132418.17);
-
 touchUp(3, 259.01, 401.38);
-
 usleep(1362734.29);
 
 touchDown(5, 1172.37, 225.50);
-
 usleep(90996.75);
-
 touchUp(5, 1172.37, 225.50);
-
 usleep(2948238.29);
 
 touchDown(5, 1147.24, 230.30);
-
 usleep(107854.58);
-
 touchUp(5, 1147.24, 230.30);
-
 usleep(2350747.12);
 
 touchDown(5, 1167.54, 201.47);
-
 usleep(116157.67);
-
 touchUp(5, 1167.54, 201.47);
-
 usleep(4203376.08);
 
 touchDown(5, 1140.48, 218.78);
-
 usleep(108140.50);
-
 touchUp(5, 1140.48, 218.78);
-
