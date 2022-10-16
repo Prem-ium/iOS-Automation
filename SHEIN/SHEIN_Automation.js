9fc148026f48078bcd0371724b45b9db6ed65137
Prem Patel <80719066+sazncode@users.noreply.github.com>
2022-10-16 18:50:30 -0400

    Create SHEIN_Automation.js

---
+++ SHEIN/SHEIN_Automation.js
@@ -0,0 +1,34 @@
+// --------------------------------------
+// Information of recording
+// Front most app: SHEIN
+// Orientation of front most app: Portrait
+// --------------------------------------
+
+// Purpose: Automate Daily Rewards/Points for SHEIN.
+const { touchDown, touchMove, touchUp, usleep, appActivate, keyDown, keyUp } = at
+
+appActivate("zzkko.com.ZZKKO");
+
+touchDown(1, 1123.08, 2514.79);
+usleep(231946.92);
+touchUp(1, 1123.08, 2514.79);
+usleep(1819898.50);
+
+touchDown(1, 1134.68, 2493.67);
+usleep(123777.67);
+touchUp(1, 1134.68, 2493.67);
+usleep(2700432.71);
+
+touchDown(6, 495.81, 548.42);
+usleep(290016.67);
+touchUp(6, 495.81, 548.42);
+usleep(3297365.17);
+
+touchDown(3, 691.04, 655.10);
+usleep(490092.67);
+touchUp(3, 691.04, 655.10);
+usleep(4668179.75);
+
+touchDown(4, 655.28, 1726.72);
+usleep(315273.79);
+touchUp(4, 655.28, 1726.72);
