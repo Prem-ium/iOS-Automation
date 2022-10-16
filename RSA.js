4d68ef9d0182d5160bf3ca14934142bf26701d1c
Prem Patel <80719066+sazncode@users.noreply.github.com>
2022-10-16 19:44:26 -0400

    Create RSA.js

---
+++ RSA.js
@@ -0,0 +1,48 @@
+// --------------------------------------
+// Information of recording
+// Time: 2022-10-10 11:15:32
+// Resolution: 1242, 2688
+// Front most app: Taco Bell
+// Orientation of front most app: Portrait
+// --------------------------------------
+
+
+const { touchDown, touchMove, touchUp, usleep, appActivate, keyDown, keyUp } = at
+
+const label = { type: CONTROLLER_TYPE.LABEL, text: "Please input the following information:" }
+const nameInput = { type: CONTROLLER_TYPE.INPUT, title: "TIKER:", key: "TIKER", value: "" }
+//const positionPicker = { type: CONTROLLER_TYPE.PICKER, title: "Position:", key: "Position", value: "CEO", options: ["CEO", "CTO", "CFO", "CXO"] }
+const public = { type: CONTROLLER_TYPE.SWITCH, title: "Use Public?:", key: "PublicBuy", value: 1 }
+const sofi = { type: CONTROLLER_TYPE.SWITCH, title: "Use SOFI?:", key: "SofiBuy", value: 1 }
+const hood = { type: CONTROLLER_TYPE.SWITCH, title: "Use Robinhood?:", key: "RobinBuy", value: 1 }
+
+
+// It's an option for users to determine weather the inputs should be remembered, if you use this control in the dialog.
+//const remember = { type: CONTROLLER_TYPE.REMEMBER, on: false }
+
+/*
+Define buttons:
+type = CONTROLLER_TYPE.BUTTON
+title = Button text
+color = Button background color, it's optional, the default value is 0x428BCA
+width = Button width upon percentage of the dialog width, it's optional, the default value is 0.5, max value is 1.0.
+flag = Integer type of button flag for identifying which button is tapped.
+collectInputs = Boolean type specifying wheather the dialog should collect the inputs while this button is tapped.
+*/
+
+const submitBtn = { type: CONTROLLER_TYPE.BUTTON, title: "Button 4", width: 1.0, flag: 1, collectInputs: true }
+
+const controls = [label, nameInput, public, sofi, hood, submitBtn]
+
+// Pop up the dialog. After popping, the script will suspend waiting for user input until any button is tapped, then returns the flag of tapped button.
+
+// What orientations the dialog could be, it's optional
+const orientations = [INTERFACE_ORIENTATION_TYPE.LANDSCAPE_LEFT, INTERFACE_ORIENTATION_TYPE.LANDSCAPE_RIGHT];
+
+const result = at.dialog({ controls, orientations });
+
+if (result == 1) {
+    alert("name:%s, birthday:%s, gender:%d", nameInput.value, positionPicker.value, developerSwitch.value)
+} else {
+    alert("Dialog returned: %s", result)
+}
