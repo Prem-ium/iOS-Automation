9f9a3467897169efb33db50e64ea78cb1eb695ec
Prem Patel <80719066+Prem-ium@users.noreply.github.com>
2023-02-12 00:19:17 -0500

    Ditching IFTT

--- charge.js
+++ charge.js
@@ -1,6 +1,7 @@
-let key = "KEY-HERE";
+let trigger = "";
+let token = "";
 
-req = new Request("https://maker.ifttt.com/trigger/ch/with/key/" + key);
+req = new Request("https://www.virtualsmarthome.xyz/url_routine_trigger/activate.php?trigger=" + trigger + "&token=" + token + "&response=smartphone");
 
 req.load();
 Script.complete();
